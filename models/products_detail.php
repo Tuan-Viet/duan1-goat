24a38cb28fb80d43880e88004563533bf12eb2f4
thanhvt1509 <92007722+thanhvt1509@users.noreply.github.com>
2022-11-18 00:14:49 +0700

    da lay cua viet ngu

--- models/products_detail.php
+++ models/products_detail.php
@@ -4,4 +4,10 @@
         $listproducts = pdo_query($sql);
         return $listproducts;
     }
+    //Lấy ra danh sách sản phẩm
+    function products_detail_all(){
+        $sql="SELECT * FROM products_detail ORDER BY id DESC";
+        $product_detail = pdo_query($sql);
+        return $product_detail;
+    }
 ?>
