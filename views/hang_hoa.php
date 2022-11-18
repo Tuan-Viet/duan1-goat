9019160cf6fe5a9a0ed751aaf32de03da69ae735
quyenncph25762 <viponline1610203@gmail.com>
2022-11-18 09:16:45 +0700

    .

--- views/hang_hoa.php
+++ views/hang_hoa.php
@@ -23,8 +23,8 @@
                         <div class="cart">
                             <a href="index.php?act=hang_hoa_chi_tiet" class="img_href">
                                 <div class="cart_photo">
-                                    <img src="//product.hstatic.net/200000136061/product/274880931_4918536151560697_4517164232254841609_n_42233f347d1b4b26b3c4847b40a37d3d_large.jpg" alt="" class="cart_img">
-                                    <img src="//product.hstatic.net/200000136061/product/274247983_366248368454609_1600481175588658934_n_104d962cc6994ce4a556a90e50186ad9_master.jpg" alt="" class="cart_img-bottom">
+                                    <img src="./images/products/AK1_xam.jpg" alt="" class="cart_img">
+                                    <img src="./images/products/AK2_den.jpg" alt="" class="cart_img-bottom">
                                 </div>
                             </a>
                             <div class="cart_nav">
@@ -40,9 +40,9 @@
                         <div class="cart">
                             <a href="" class="img_href">
                                 <div class="cart_photo">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983820729_fc17d8bb3320f21b4f65c79a72988f4f_1e8f22cfdc2c40b4a63476898704a225_master.jpg" alt="" class="cart_img">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983823182_d8dd2ed64352257d89beaed48b4a3439_d3695a5a10204b7d9c6999b64dbbcd93_master.jpg" alt="" class="cart_img-bottom">
-                            </div>
+                                    <img src="./images/products/AK1_xam.jpg" alt="" class="cart_img">
+                                    <img src="./images/products/AK2_den.jpg" alt="" class="cart_img-bottom">
+                                </div>
                             </a>
                             <div class="cart_nav">
                                 <p class="cart_name">Jacket Basic SS2</p>
@@ -57,9 +57,9 @@
                         <div class="cart">
                             <a href="" class="img_href">
                                 <div class="cart_photo">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983820729_fc17d8bb3320f21b4f65c79a72988f4f_1e8f22cfdc2c40b4a63476898704a225_master.jpg" alt="" class="cart_img">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983823182_d8dd2ed64352257d89beaed48b4a3439_d3695a5a10204b7d9c6999b64dbbcd93_master.jpg" alt="" class="cart_img-bottom">
-                            </div>
+                                    <img src="./images/products/AK1_xam.jpg" alt="" class="cart_img">
+                                    <img src="./images/products/AK2_den.jpg" alt="" class="cart_img-bottom">
+                                </div>
                             </a>
                             <div class="cart_nav">
                                 <p class="cart_name">Jacket Basic SS2</p>
@@ -74,9 +74,9 @@
                         <div class="cart">
                             <a href="" class="img_href">
                                 <div class="cart_photo">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983820729_fc17d8bb3320f21b4f65c79a72988f4f_1e8f22cfdc2c40b4a63476898704a225_master.jpg" alt="" class="cart_img">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983823182_d8dd2ed64352257d89beaed48b4a3439_d3695a5a10204b7d9c6999b64dbbcd93_master.jpg" alt="" class="cart_img-bottom">
-                            </div>
+                                    <img src="./images/products/AT1_xam.jpg" alt="" class="cart_img">
+                                    <img src="./images/products/AT1_do.jpg" alt="" class="cart_img-bottom">
+                                </div>
                             </a>
                             <div class="cart_nav">
                                 <p class="cart_name">Jacket Basic SS2</p>
@@ -91,25 +91,10 @@
                         <div class="cart">
                             <a href="" class="img_href">
                                 <div class="cart_photo">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983820729_fc17d8bb3320f21b4f65c79a72988f4f_1e8f22cfdc2c40b4a63476898704a225_master.jpg" alt="" class="cart_img">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983823182_d8dd2ed64352257d89beaed48b4a3439_d3695a5a10204b7d9c6999b64dbbcd93_master.jpg" alt="" class="cart_img-bottom">
-                            </div>
-                            </a>
-                            <div class="cart_nav">
-                                <p class="cart_name">Jacket Basic SS2</p>
-                                <p class="cart_price">225,000đ <del class="sale" style="color:#6666">450,000đ</del></p>
-                            </div>
-                            <ul class="cart_color mt8">
-                                <li class="not_swap"><span class="black"></span></li>
-                                <li class="not_swap"><span class="black"></span></li>
-                                <li class="not_swap"><span class="black"></span></li>
-                            </ul>
-                        </div>
-                        <div class="cart">
-                            <a href="" class="img_href">
                                 <div class="cart_photo">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983820729_fc17d8bb3320f21b4f65c79a72988f4f_1e8f22cfdc2c40b4a63476898704a225_master.jpg" alt="" class="cart_img">
-                                <img src="//product.hstatic.net/200000136061/product/z3231983823182_d8dd2ed64352257d89beaed48b4a3439_d3695a5a10204b7d9c6999b64dbbcd93_master.jpg" alt="" class="cart_img-bottom">
+                                    <img src="./images/products/AK2_nau.jpg" alt="" class="cart_img">
+                                    <img src="./images/products/AK2_xanh.jpg" alt="" class="cart_img-bottom">
+                                </div>
                             </div>
                             </a>
                             <div class="cart_nav">
