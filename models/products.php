9f6234bf227d490f7289c8e6e090e978eee7cacf
thanhvt1509 <92007722+thanhvt1509@users.noreply.github.com>
2022-12-05 00:24:38 +0700

    05/12/2022

--- models/products.php
+++ models/products.php
@@ -49,18 +49,6 @@
         $listhanghoa = pdo_query($sql);
         return $listhanghoa;
     }
- //Lấy ra danh sách sản phẩm
- function products_all(){
-    $sql="SELECT * FROM products ORDER BY id DESC";
-    $hanghoa = pdo_query($sql);
-    return $hanghoa;
-}
-//Lấy ra 1 sản phẩm
-function product_one($id){
-    $sql="SELECT * FROM products WHERE id=$id";
-    $hanghoa = pdo_query($sql);
-    return $hanghoa;
-}
 //Xóa sản phẩm
 function delete_product($id){
     $sql = "DELETE FROM products WHERE id ='$id'";
